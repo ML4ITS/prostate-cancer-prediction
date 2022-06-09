125b2c88a8e5a2cd3d615cfa66625df946a256e8
elitedde <79220600+elitedde@users.noreply.github.com>
2022-06-09 13:15:15 +0200

    modify config_file

--- Models_2/config_file/config_parser.py
+++ Models_2/config_file/config_parser.py
@@ -6,9 +6,8 @@ class Data:
         with open("config_file/parameters.yaml", 'r') as stream:
             try:
                 p = yaml.safe_load(stream)
-
                 self.trials = p["trials"]
                 self.epochs = p["epochs"]
-
+                self.balanced = p["balanced"]
             except yaml.YAMLError as exc:
                 print(exc)
