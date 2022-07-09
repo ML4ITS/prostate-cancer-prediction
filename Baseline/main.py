27a11b05cfc0aa5141ee7b6941e51ca591b8dd11
elitedde <79220600+elitedde@users.noreply.github.com>
2022-07-09 10:26:06 +0200

    Update main.py

--- Baseline/main.py
+++ Baseline/main.py
@@ -1,8 +1,6 @@
 import sys
 sys.path.append('../')
 from utils import *
-from methods.LSTM import training_test_LSTM
-import time
 from preprocessing import *
 from config_file.config_parser import Data
 from manage_dataset import *
@@ -10,7 +8,6 @@ from settings import p
 from methods.sklearn_classifiers import *
 
 if __name__ == "__main__":
-    start = time.time()
     #parameters from command line
     _, path1, path2 = sys.argv
     m = Data() #get parameters
@@ -18,10 +15,9 @@ if __name__ == "__main__":
     p.regularization = False #set regularization parameter
     #baseline 1 val1 = 2 val2 = 1
     #baseline 3 val1 = 3 val2 = 2
-    for baseline in range(1, 3):
+    for baseline in range(1, 4):
         data1, data2 = upload_db(path1, path2, "days", len=4)
-        # data1, data2 = remove_outliers(data1), remove_outliers(data2)
-        data2 = balance_db(data1, data2, m.balanced)
+        data1, data2 = remove_outliers(data1), remove_outliers(data2)
         if baseline == 1:
             data1, data2 = extract_last_value_wrapp(data1, data2, val1=2, val2=1)
         if baseline == 3:
@@ -37,7 +33,13 @@ if __name__ == "__main__":
         case = "results/case" + str(baseline)
         plot_isomap(data1, data2, case)
         data = concat_data(data1, data2)
-        print(data.head())
         heatmap(data, case)
-        classifiers(data, case, m.repetition)
+        data, test = extract_unbalanced_test(data, data1)
+        train = extract_balanced_train(data, m.balanced, id = 1)
+        train, test = remove_id(train, test)
+        print("Shape train: " + str(train.shape))
+        print("Shape test: " + str(test.shape))
+        # same db every time
+        classifiers(train, test, case, m.repetition)
+        print("---------Next baseline-----------")
 
