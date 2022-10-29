d19a64078d61739ab2c04f95bd7966bd68fae1c0
elitedde <79220600+elitedde@users.noreply.github.com>
2022-10-29 21:04:25 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
 class p(object):
+    #two approcches: time series regularization and irregular time series
     regularization = None
-
+    #if regularization is True, interpolation or zero-filling is applied
     interpolation = None
-
+    #indicator means missing value indicators
     indicator = None
-
+    #one of the machine learning methods for the second approach is multiple kernels learning 
     multiple_kernels = None
 
 
