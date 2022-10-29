9785326a327b73fb26448b675f2b39eb656516b3
elitedde <79220600+elitedde@users.noreply.github.com>
2022-10-29 21:27:05 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,10 +12,13 @@ from sklearn.manifold import Isomap
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.metrics import confusion_matrix
 from sklearn.inspection import permutation_importance
+from settings import p
 
 dispatcher= {'nn.Tanh()': nn.Tanh(), 'nn.ReLU()': nn.ReLU(), 'nn.ELU()': nn.ELU(), 'nn.LeakyReLU()': nn.LeakyReLU(), 'nn.Sigmoid()': nn.Sigmoid()}
 
 def get_random_numbers(layers, trial, min, max, element, int = True, desc = True, step = 1):
+    #hyperparameter tuning needs to generate random numbers for the number of layers, hidden neurons, dropout etc.
+    #return the list of the generated values for a given hyperparameter
     random_float_list = []
     for i in range(layers+1):
         el = element + str(i)
@@ -27,9 +30,11 @@ def get_random_numbers(layers, trial, min, max, element, int = True, desc = True
     return np.sort(np.array(random_float_list))[::-1] if desc else np.sort(np.array(random_float_list))
 
 def get_kernel_size(input_size, output_size,padding):
+    #return the kernel size from the given parameters: input size, output size and padding
     return 2 * padding + input_size + 1 - output_size
 
 def save_evaluation_metric(model, accuracy, f1score, specificity, case):
+    #accuracy, f1score and specificity are saved in a file for a given model
     file = open(model+ "/" + case +"/results.txt", "a+")
     accuracy = "accuracy: " + str(accuracy.item()) + "\n"
     f1score = "f1score: " + str(f1score.item()) + "\n"
@@ -40,17 +45,20 @@ def save_evaluation_metric(model, accuracy, f1score, specificity, case):
     file.close()
 
 def save_hyperparameter(model, study, case):
+    #the selected hyperparameters are saved in a file
     file = open(model + case + "/results.txt", "a+")
     content = study.best_params
     file.write(str(content) + "\n")
     file.close()
 
 def flush_file(model, case):
+    #clear the internal buffer of the file
     fo = open(model+ "/" + case +"/results.txt", "wb")
     fo.flush()
     fo.close()
 
 def get_std_mean_accuracy(model, accuracy, case):
+    #the mean and the standard deviation for the accuracy have been saved in the file
     file = open(model+ "/" + case +"/results.txt", "a+")
     file.seek(0)
     mean = "mean accuracy: " + str(np.mean(accuracy)) + "\n"
@@ -69,6 +77,7 @@ def plot_accuracy_loss(model,trainer,case):
     plt.savefig(model+"/" +case + "/table.png")
 
 def str2bool(v):
+    #the function converts the given input into a boolean value
     if isinstance(v, bool):
         return v
     if v.lower() in ('yes', 'true', 'True', 't', 'y', '1'):
@@ -80,14 +89,17 @@ def str2bool(v):
 
 
 def multiple_boxplot_models(accuracy, case):
+    #bloxplot using accuracy paramaters for the irregular time series approach
     # Pandas dataframe
-    data = pd.DataFrame({"CNN1D": accuracy[0], "MLP": accuracy[1], "LSTM": accuracy[2], "MULTIPLE_KERN": accuracy[3]})
+    data = pd.DataFrame({"CASE1": accuracy[0], "CASE2": accuracy[1], "CASE3": accuracy[2], "CASE4": accuracy[3]})
     # Plot the dataframe
-    ax = data[['CNN1D', 'MLP', 'LSTM', 'MULTIPLE_KERN']].plot(kind='box', title='boxplot')
+    ax = data[["CASE1", "CASE2", "CASE3", "CASE4"]].plot(kind='box', title='boxplot')
+    plt.show()
     # Display the plot
-    plt.savefig("boxplot/" + case + "/table.png")
+    # plt.savefig("boxplot/" + case + "/table.png")
 
 def multiple_boxplot_inputs(accuracy, path):
+    #bloxplot using accuracy paramaters for the time series regularization approach
     # Pandas dataframe
     data = pd.DataFrame({"NO_INTERP": accuracy[2], "NO_INTERP+INDIC": accuracy[3], "INTERP": accuracy[1], "INTERP+INDIC": accuracy[0] })
     # Plot the dataframe
@@ -97,8 +109,11 @@ def multiple_boxplot_inputs(accuracy, path):
     plt.savefig(path + "_boxplot.png")
 
 def get_binary_indicators(x):
+    #given the input, the negative values indicate the not real values while the positive values the real values.
+    #then the binary missing vector is extracted
     preds = (x < 0).float()
-    x = np.where(x<0, -x, x)
+    if p.interpolation:
+        x = np.where(x<0, -x, x)
     x = np.hstack((x.reshape(-1,1), preds.reshape(-1,1))).reshape(x.shape[1], 2)
     return torch.FloatTensor(x)
 
@@ -116,6 +131,7 @@ def combinations(i):
     return str2bool(x), str2bool(y)
 
 def plot_isomap(data1, data2, baseline):
+    #the function plot the isomap to understand the distribution of the data
     embedding = Isomap(n_components = 2)
     dat = pd.concat([data1.iloc[:50, :-1], data2.iloc[:50, :-1]])
     color = pd.concat([data1.iloc[:50, -1], data2.iloc[:50, -1]])
@@ -126,11 +142,13 @@ def plot_isomap(data1, data2, baseline):
     plt.savefig(baseline + "/isomap")
 
 def heatmap(data, baseline):
+    #the function plots the heatmap
     plt.figure(figsize = (9,9))
     sn.heatmap(data = data.corr().round(2), cmap = "coolwarm", linewidth = .5, annot = True, annot_kws = {"size":12})
     plt.savefig(baseline + "/heatmap")
 
 def feature_importance(X_test, y_test, model, baseline, name):
+    #the function calls the permutation importance algorithm to understand which features are the most important for the prediction of the test set
     feature_names = [f"feature{i}" for i in range(X_test.shape[1])]
     result = permutation_importance(model, X_test, y_test, n_repeats = 10, random_state = 42, n_jobs = 2)
     importances = pd.Series(result.importances_mean, index = feature_names)
@@ -142,6 +160,7 @@ def feature_importance(X_test, y_test, model, baseline, name):
     plt.savefig(baseline + "/feature_importance_"+ name)
 
 def conf_matrix_categ(file, df, model, feature1, feature2, inf, sup, case, feature3 = None, feature4 = None):
+    #the results (f1score, confusion mantrix, accuracy) are saved in a file for each type categories
     mean, median, mode = None, None, None
     if sup == -1:
         c = df.loc[(df[feature1] == inf)]
@@ -172,6 +191,7 @@ def conf_matrix_categ(file, df, model, feature1, feature2, inf, sup, case, featu
         file.write("No results for this category\n")
 
 def save_result_df(model, target, preds, case):
+    #the performance is extracted using the different categories: visits, age and cancer risk
     file = open(model + "/" + case + "/categories/results.txt", "w")
     df = pd.read_csv("df.csv")
     df["target"] = np.array(target).astype(int)
@@ -193,7 +213,6 @@ def save_result_df(model, target, preds, case):
     conf_matrix_categ(file, df, model, "min_age", "max_age", 55, 65, case, feature3=65, feature4=75)
     conf_matrix_categ(file, df, model, "min_age", "max_age", 55, 65, case, feature3=75, feature4=85)
     conf_matrix_categ(file, df, model, "min_age", "max_age", 65, 75, case, feature3=75, feature4=85)
-
     #risk category
     df = df.loc[(df["target"] == 1)]
     for i in range(6):
